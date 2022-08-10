34c6d26fc3c91b6ea171be82fa198acf146ee0e3
Patricia Tami Sakita <tamisakita@hotmail.com>
2022-08-09 21:26:53 -0300

    Initial commit

---
+++ src/test/java/com/fiap/footballapi/FootballApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.footballapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
