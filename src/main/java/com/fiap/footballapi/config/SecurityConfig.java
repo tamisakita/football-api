e81814ecf597bfb51ce0d0534b2869e7ecfc7c3b
Patricia Tami Sakita <tamisakita@hotmail.com>
2022-08-20 15:46:07 -0300

    add collection

--- src/main/java/com/fiap/footballapi/config/SecurityConfig.java
+++ src/main/java/com/fiap/footballapi/config/SecurityConfig.java
@@ -4,17 +4,13 @@ import com.fiap.footballapi.security.JwtEntryPoint;
 import com.fiap.footballapi.security.JwtRequestFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
